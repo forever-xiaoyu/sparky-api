960eb14b49f8c8b631fc5357ef88d7b6197a5bd2
yuc <yuc@lawsdata.com>
2026-03-04 17:28:27 +0800

    feat: 添加ts类型推导配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // 没有附带 .d.ts 类型声明文件，其他项目引用时无法类型推导
+    dts({
+      insertTypesEntry: true, // 自动生成类型入口文件，保证 npm 包类型可正确推导
+      include: ['src/**/*.ts', 'src/**/*.d.ts', 'src/**/*.vue']
+    })
   ],
   resolve: {
     alias: {
@@ -18,16 +24,16 @@ export default defineConfig({
       entry: './src/index.ts', // 入口文件
       name: 'sparkyie/api',
       fileName: 'sparkyie-api',
-      formats: ['es'],
+      formats: ['es']
     },
     rollupOptions: {
       external: ['vue'],
       output: {
         globals: {
-          vue: 'Vue',
-        },
-      },
-    },
+          vue: 'Vue'
+        }
+      }
+    }
   },
   server: {
     proxy: {
@@ -37,5 +43,5 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '') // 重写路径，去掉 /api 前缀
       }
     }
-  },
+  }
 })
