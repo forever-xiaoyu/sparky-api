118b22e58a71ab862ee08a11406892df8e286b7f
forever-xiaoyu <yc609.cn@gmail.com>
2026-08-10 15:26:47 +0800

    feat: 移除vite-plugin-dts插件配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    // 没有附带 .d.ts 类型声明文件，其他项目引用时无法类型推导
-    dts({
-      insertTypesEntry: true, // 自动生成类型入口文件，保证 npm 包类型可正确推导
-      include: ['src/**/*.ts', 'src/**/*.d.ts', 'src/**/*.vue']
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
